4c148ca56ad64d23af75fa6af1b6bcce37d4084b
FireCube <etiennedx00@gmail.com>
2019-01-17 15:51:00 +0100

    Added the 3 motors but more testing required

--- robotic_arm_v1.ino
+++ robotic_arm_v1.ino
@@ -1,27 +1,54 @@
 #include "motor.h"
 
-const int button1 = 5;
-const int button2 = 6;
+const int buttons[6] = {2, 3, 4, 5, 6, 7};
 
-Motor baseRotation(3, 4, 200 * 8);
+Motor baseRotation(13, 12, 200 * 8);
+Motor baseY(11, 10, 200 * 8);
+Motor centre(8, 9, 200 * 8);
 
 void setup()
 {
   baseRotation.Init();
-  pinMode(button1, INPUT); 
-  pinMode(button2, INPUT);
+  baseY.Init();
+  centre.Init();
+  for(int i = 0; i < 6; i++)
+  {
+    pinMode(buttons[i], INPUT);
+  }
 }
 
 void loop()
-{
-  if(digitalRead(button1))
+{baseRotation.Step(10); delay(200);
+  /*if(digitalRead(buttons[0]))
   {
     baseRotation.SetDir(HIGH);
     baseRotation.Step();
   }
-  else if(digitalRead(button2))
+  else if(digitalRead(buttons[1]))
   {
     baseRotation.SetDir(LOW);
     baseRotation.Step();
   }
+  
+  if(digitalRead(buttons[2]))
+  {
+    baseY.SetDir(HIGH);
+    baseY.Step();
+  }
+  else if(digitalRead(buttons[3]))
+  {
+    baseY.SetDir(LOW);
+    baseY.Step();
+  }
+  
+  if(digitalRead(buttons[4]))
+  {
+    centre.SetDir(HIGH);
+    centre.Step();
+  }
+  else if(digitalRead(buttons[5]))
+  {
+    centre.SetDir(LOW);
+    centre.Step();
+  }*/
 }
